29ee00e8d0c545c5bcf7b39376cbec70f403f86a
hondo-s <hondo-s@klab.com>
2018-06-21 00:22:12 +0900

    refactor

--- src/Main.ts
+++ src/Main.ts
@@ -4,41 +4,60 @@ import * as _ from 'underscore/underscore-min.js'
 import * as graph from './miserables.json'
 
 export class Main {
+    svg = null
+    readonly padding = 100
+    width = 0
+    height = 0
+    readonly domainX = [500, -500]
+    readonly domainY = [-50, 50]
+
     constructor () {
+        this.initSvg()
+        this.drawCoordinates()
+    }
+
+    initSvg () {
         const visualization = document.getElementById("visualization")
 
-        const padding = 100
-        const width = visualization.clientWidth
-        const height = width
+        this.width = visualization.clientWidth
+        this.height = this.width
 
-        const g = d3.select("#visualization").append("svg:svg")
-            .attr('width', width)
-            .attr('height', height)
+        this.svg = d3.select("#visualization").append("svg:svg")
+            .attr('width', this.width)
+            .attr('height', this.height)
+    }
 
-        const xScale = d3.scaleLinear().domain([10, -10]).range([width - padding, padding])
-        const yScale = d3.scaleLinear().domain([-10, 10]).range([height - padding, padding])
+    drawCoordinates() {
+        const xScale = d3.scaleLinear().domain(this.domainX).range([this.width - this.padding, this.padding])
+        const yScale = d3.scaleLinear().domain(this.domainY).range([this.height - this.padding, this.padding])
 
-        const xAxis = d3.axisTop(xScale).tickValues(_.range(10, -11).filter(d => d%2===0))
-        const yAxis = d3.axisLeft(yScale).tickValues(_.range(-10, 11).filter(d => d%2===0))
+        const stepX = Math.abs(this.domainX[0] - this.domainX[1]) / 10
+        const stepY = Math.abs(this.domainY[0] - this.domainY[1]) / 10
+        const xAxis = d3.axisTop(xScale).tickValues(_.range(this.domainX[0], (this.domainX[1] < this.domainX[0]) ? this.domainX[1]-1 : this.domainX[1]+1).filter(d => d%stepX===0))
+        const yAxis = d3.axisLeft(yScale).tickValues(_.range(this.domainY[0], (this.domainY[1] < this.domainY[0]) ? this.domainY[1]-1 : this.domainY[1]+1).filter(d => d%stepY===0))
 
         // --
-        const xAxisPlot = g.append('g')
+        const xAxisPlot = this.svg.append('g')
             .attr("class", "axis axis--x")
-            .attr('transform', `translate(0, ${height/2})`)
+            .attr('transform', `translate(0, ${this.height/2})`)
             .call(xAxis)
 
         // |
-        const yAxisPlot = g.append('g')
+        const yAxisPlot = this.svg.append('g')
             .attr("class", "axis axis--y")
-            .attr('transform', `translate(${width/2}, 0)`)
+            .attr('transform', `translate(${this.width/2}, 0)`)
             .call(yAxis)
 
         xAxisPlot.selectAll(".tick line")
-    		.attr("y1", (width - (2*padding))/2 * -1)
-    		.attr("y2", (width - (2*padding))/2 * 1);
+            .attr("y1", (this.width - (2*this.padding))/2 * -1)
+            .attr("y2", (this.width - (2*this.padding))/2 * 1);
 
-    	yAxisPlot.selectAll(".tick line")
-    		.attr("x1", (width - (2*padding))/2 * -1)
-    		.attr("x2", (width - (2*padding))/2 * 1);
+        yAxisPlot.selectAll(".tick line")
+            .attr("x1", (this.width - (2*this.padding))/2 * -1)
+            .attr("x2", (this.width - (2*this.padding))/2 * 1);
+    }
+
+    drawCircle() {
+
     }
 }
