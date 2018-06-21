c58bca5a87f407e9f8f275f4f9e496f2560c2c5b
hondo-s <hondo-s@klab.com>
2018-06-21 23:37:31 +0900

    graph deepcopy & auto resize

--- src/Main.ts
+++ src/Main.ts
@@ -1,23 +1,43 @@
 import * as PIXI from 'pixi.js/dist/pixi.min.js'
 import * as d3 from 'd3/dist/d3.min.js'
 import * as _ from 'underscore/underscore-min.js'
-import * as graph from './philosophers.json'
+import * as originalGraph from './philosophers.json'
 
 export class Main {
     svg = null
+    graph = {}
     readonly padding = 100
     width = 0
     height = 0
     sf = 1 // Scale factor for responsive svg
-    nodeRadius = 60
+    readonly nodeRadius = 60
+    scaledNodeRadius = 60
 
     constructor () {
         this.initSvg()
         this.normalizeCoords()
         this.drawGraph()
+
+        this.initResizeEndCallback()
+    }
+
+    initResizeEndCallback() {
+        var timer = 0;
+
+        window.onresize = () => {
+            if (timer > 0) {
+                clearTimeout(timer);
+            }
+
+            timer = setTimeout(() => {
+                console.log("Resize End")
+                this.onResizeEnd()
+            }, 300)
+        }
     }
 
     initSvg () {
+        this.graph = JSON.parse(JSON.stringify(originalGraph))
         const visualization = document.getElementById("visualization")
 
         this.width = visualization.clientWidth
@@ -27,6 +47,9 @@ export class Main {
         // 実際のwidthに合わせるためにすべての座標に↓をかける.
         this.sf = this.width / 1000
 
+        if (this.svg !== null) {
+            this.svg.remove()
+        }
 
         this.svg = d3.select("#visualization").append("svg")
             .attr("xmlns", "http://www.w3.org/2000/svg")
@@ -35,24 +58,19 @@ export class Main {
     }
 
     normalizeCoords() {
-        for (let i=0; i<graph.nodes.length; i++) {
-            graph.nodes[i].x *= this.sf
-            graph.nodes[i].y *= this.sf
+        for (let i=0; i<this.graph.nodes.length; i++) {
+            this.graph.nodes[i].x *= this.sf
+            this.graph.nodes[i].y *= this.sf
         }
 
-        this.nodeRadius *= this.sf
+        this.scaledNodeRadius = this.nodeRadius * this.sf
     }
 
     drawGraph() {
-        const colour = (function() {
-            let scale = d3.scaleOrdinal(d3.schemeSet3);
-            return (num) => parseInt(scale(num).slice(1), 16);
-        })()
-
         const links = this.svg.append("g").attr("class", "links").selectAll("links")
-        graph.links.forEach(l => {
-            let src = graph.nodes[l.source]
-            let tgt = graph.nodes[l.target]
+        this.graph.links.forEach(l => {
+            let src = this.graph.nodes[l.source]
+            let tgt = this.graph.nodes[l.target]
             links.data([l])
                 .enter()
                 .append("line")
@@ -65,7 +83,7 @@ export class Main {
         })
 
         const nodes = this.svg.append("g").attr("class", "nodes").selectAll("nodes")
-            .data(graph.nodes)
+            .data(this.graph.nodes)
             .enter()
             .append("a")
             .attr("target", "_blank")
@@ -78,14 +96,14 @@ export class Main {
             .append("circle")
             .attr("cx", d => d.x)
             .attr("cy", d => d.y)
-            .attr("r", this.nodeRadius)
+            .attr("r", this.scaledNodeRadius)
             .attr("fill", "white")
             .style("stroke", "black")
 
         nodes
             .append("text")
             .attr("x", d => d.x)
-            .attr("y", d => d.y - this.nodeRadius - (10 * this.sf))
+            .attr("y", d => d.y - this.scaledNodeRadius - (10 * this.sf))
             .attr("stroke", "black")
             .attr("text-anchor", "middle")
             .text(d => d.name)
@@ -93,17 +111,23 @@ export class Main {
         nodes.append("clipPath")
             .attr("id", (d, i) => `clipCircle${i}`)
             .append("circle")
-            .attr("r", this.nodeRadius - (10 * this.sf))
+            .attr("r", this.scaledNodeRadius - (10 * this.sf))
             .attr("cx", d => d.x)
             .attr("cy", d => d.y)
 
         nodes
             .append("svg:image")
-            .attr("x", d => d.x - this.nodeRadius)
-            .attr("y", d => d.y - this.nodeRadius)
-            .attr("width", this.nodeRadius * 2)
-            .attr("height", this.nodeRadius * 2)
+            .attr("x", d => d.x - this.scaledNodeRadius)
+            .attr("y", d => d.y - this.scaledNodeRadius)
+            .attr("width", this.scaledNodeRadius * 2)
+            .attr("height", this.scaledNodeRadius * 2)
             .attr("xlink:href", d => d.img)
             .attr("clip-path", (d, i) => `url(#clipCircle${i})`)
+    }
+
+    onResizeEnd () {
+        this.initSvg()
+        this.normalizeCoords()
+        this.drawGraph()
     }
 }
