ccfdc325110f47051498d53cc9bd7b3222410ef4
hondo-s <hondo-s@klab.com>
2018-06-21 22:33:08 +0900

    tappable nodes

--- src/Main.ts
+++ src/Main.ts
@@ -1,16 +1,19 @@
 import * as PIXI from 'pixi.js/dist/pixi.min.js'
 import * as d3 from 'd3/dist/d3.min.js'
 import * as _ from 'underscore/underscore-min.js'
-import * as graph from './miserables.json'
+import * as graph from './philosophers.json'
 
 export class Main {
     svg = null
     readonly padding = 100
     width = 0
     height = 0
+    sf = 1 // Scale factor for responsive svg
+    nodeRadius = 60
 
     constructor () {
         this.initSvg()
+        this.normalizeCoords()
         this.drawGraph()
     }
 
@@ -20,9 +23,24 @@ export class Main {
         this.width = visualization.clientWidth
         this.height = this.width
 
-        this.svg = d3.select("#visualization").append("svg:svg")
+        // widthが1000と仮定してレイアウトする.
+        // 実際のwidthに合わせるためにすべての座標に↓をかける.
+        this.sf = this.width / 1000
+
+
+        this.svg = d3.select("#visualization").append("svg")
+            .attr("xmlns", "http://www.w3.org/2000/svg")
             .attr('width', this.width)
             .attr('height', this.height)
+    }
+
+    normalizeCoords() {
+        for (let i=0; i<graph.nodes.length; i++) {
+            graph.nodes[i].x *= this.sf
+            graph.nodes[i].y *= this.sf
+        }
+
+        this.nodeRadius *= this.sf
     }
 
     drawGraph() {
@@ -30,20 +48,6 @@ export class Main {
             let scale = d3.scaleOrdinal(d3.schemeSet3);
             return (num) => parseInt(scale(num).slice(1), 16);
         })()
-
-        graph = {
-          "nodes": [
-            {"group": 1, "name": "Hannah Arendt", "x": 100, "y": 100, "img": "https://upload.wikimedia.org/wikipedia/commons/6/62/Hannah_arendt-150x150.jpg"},
-            {"group": 2, "name": "Michel Foucault", "x": 500, "y": 100, "img": "https://upload.wikimedia.org/wikipedia/en/5/52/Foucault5.jpg"},
-            {"group": 3, "name": "Alfred North Whitehead", "x": 300, "y": 500, "img": "https://upload.wikimedia.org/wikipedia/en/4/4e/Alfred_North_Whitehead.jpg"},
-          ],
-          "links": [
-            {"source": 0, "target": 1, "value": 1},
-            {"source": 1, "target": 2, "value": 2},
-            {"source": 2, "target": 0, "value": 3},
-          ]
-        }
-
 
         const links = this.svg.append("g").attr("class", "links").selectAll("links")
         graph.links.forEach(l => {
@@ -63,21 +67,25 @@ export class Main {
         const nodes = this.svg.append("g").attr("class", "nodes").selectAll("nodes")
             .data(graph.nodes)
             .enter()
+            .append("a")
+            .attr("target", "_blank")
+            .attr("xlink:href", d => d.url)
             .append("g")
             .attr("class", "node")
+
         nodes
             .append("circle")
             .attr("class", "node")
             .attr("cx", d => d.x)
             .attr("cy", d => d.y)
-            .attr("r", 60)
+            .attr("r", this.nodeRadius)
             .attr("fill", "white")
             .style("stroke", "black")
 
         nodes
             .append("text")
             .attr("x", d => d.x)
-            .attr("y", d => d.y - 70)
+            .attr("y", d => d.y - this.nodeRadius - (10 * this.sf))
             .attr("stroke", "black")
             .attr("text-anchor", "middle")
             .text(d => d.name)
@@ -85,16 +93,16 @@ export class Main {
         nodes.append("clipPath")
             .attr("id", (d, i) => `clipCircle${i}`)
             .append("circle")
-            .attr("r", 50)
+            .attr("r", this.nodeRadius - (10 * this.sf))
             .attr("cx", d => d.x)
             .attr("cy", d => d.y)
 
         nodes
             .append("svg:image")
-            .attr("x", d => d.x - 50)
-            .attr("y", d => d.y - 50)
-            .attr("width", d => 120)
-            .attr("height", d => 120)
+            .attr("x", d => d.x - this.nodeRadius)
+            .attr("y", d => d.y - this.nodeRadius)
+            .attr("width", this.nodeRadius * 2)
+            .attr("height", this.nodeRadius * 2)
             .attr("xlink:href", d => d.img)
             .attr("clip-path", (d, i) => `url(#clipCircle${i})`)
     }
